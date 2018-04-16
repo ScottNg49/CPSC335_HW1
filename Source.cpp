1975e9f386f38092b9248148c6134cf13ccba423
LuiRangel <xxcmgxx02@gmail.com>
2018-04-15 18:02:13 -0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,114 +1,29 @@
 #include <iostream>
+#include <stdio.h>
 #include <string>
-#include <sstream>
-#include "Bubblesort.h"
-
+#include "Header.h"
 using namespace std;
-
 const int MAX_INT = 12;
+const int MAX_STR = 24;
 
-class RaceMgr  
-{
-	BubbleSort BS;
-	int bubbleSortCallCount = 0;
-	int mergeSortCallCount = 0;
-	int bsTurnsItTook = 0;
-	int msTurnsItTook = 0;
-public:
-	// Member Functions()
-	RaceMgr(int argv_arry[], int n);
-	void run();
-	void print(int argv_arry[], int n);
-};
-
-RaceMgr::RaceMgr(int argv_arry[], int n) { // n is size of the array
-	
-	BS.populate(argv_arry, n);
-	//MS.populate(argv_arry, n);
-
-}
-
-void RaceMgr::print(int argv_arry[], int n) {
-
-	for (int i = 0; i < n; i++)
-	{
-		cout << argv_arry[i] << ", ";
-	}
-}
-
-void RaceMgr::run()
-{
-	//0 is not done
-	//1 is done
-	//2 valued moved
-
-		while (BS.get_state != 1 /*|| !MS.Done*/) {
-
-			if (BS.get_state != 1) {
-
-				BS.step();
-
-				if(BS.get_state == 2){
-					cout << "#" << BS.get_step_count << ",  BS:  [";
-					//print();
-					cout << "]" << endl;
-				}
-			}/*
-			if (!MS.is_done()) {
-				MS.step();
-				msTurnsItTook++;
-				mergeSortCallCount++;
-				//if(moved){
-					cout << "#" << mergeSortCallCount << ",  MS:  [";
-					//print();
-					cout << "]" << endl;
-				}
-			}*/
-		}
-		cout << "BubbleSort took " << BS.get_step_count << " turns to finish." << endl;
-		cout << "MergeSort took " << msTurnsItTook << " turns to finish." << endl;
-}
-
-
-int main() {
-	int myarry[MAX_INT];
+int main(int argc, const char* argv[]) {
 	string InputArray;
+	int SortArray[MAX_INT] = { 14, 7, 88, 1, 6, 2, 9, 13, 24, 3, 11, 56};
+	//int cnt = 0;
+	//cout << "Enter Integer array to be sorted (MAX: 12): \n";
+	//getline(cin, InputArray);
 
-	cout << "Enter Integer array to be sorted (MAX: 12): ";
-	getline(cin, InputArray);
+	MergeSortAlg MS;
+	MS.populate(SortArray, MAX_INT);
 
-	stringstream ss(InputArray);
-	string s;
-	int array_size = 0;
-	
-	while (getline(ss, s, ' ')) {
-		myarry[array_size] = stoi(s);
-		array_size++;
-	}
+	cout << endl << MS.step() << endl;
+	cout << endl << MS.step() << endl;
+	cout << endl << MS.step() << endl;
+	cout << endl << MS.step() << endl;
+	cout << endl << MS.step() << endl;
+	cout << endl << MS.step() << endl;
 
-	RaceMgr Mgr(myarry, array_size);
-	Mgr.run();
-
+	//MergeSortAlg::MergeSortAlg::populate(*SortArray, MAX_INT);
 	system("pause");
 	return 0;
 }
-
-/*void RaceMgr::run()
-{
-	while(BS.get_state() != 1)
-		if(BS.get_state() != 1)
-		{
-			BS.step();
-			if(BS.get_state() == 2)
-				print(BS.get_array(), BS.get_step_count(), BS.get_user_size());
-		};
-};*/
-
-/*
-void RaceMgr::print(int *argv_array, int step_count, int array_size)
-{
-	cout << "# " << step_count << " BubbleSort: [";
-	for(int i = 0; i < array_size; i++)
-		cout << argv_array[i] << ' ';
-	cout << "]\n\n";
-};*/
