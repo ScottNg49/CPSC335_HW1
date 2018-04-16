92c4ab007b8f60a5feacb3dfac1a1dd259be7ad2
kitkav <meghparakavit@gmail.com>
2018-04-15 17:43:46 -0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -6,72 +6,85 @@
 using namespace std;
 
 const int MAX_INT = 12;
-const int MAX_STR = 24;
 
 class RaceMgr  
 {
 	BubbleSort BS;
-
+	int bubbleSortCallCount = 0;
+	int mergeSortCallCount = 0;
+	int bsTurnsItTook = 0;
+	int msTurnsItTook = 0;
 public:
 	// Member Functions()
 	RaceMgr(int argv_arry[], int n);
 	void run();
-	
+	void print(int argv_arry[], int n);
 };
-
 
 RaceMgr::RaceMgr(int argv_arry[], int n) { // n is size of the array
 	
 	BS.populate(argv_arry, n);
 	//MS.populate(argv_arry, n);
-	/*MergeSort  MS(argv_arry[], n); 
-	
-	*/
+
 }
 
+void RaceMgr::print(int argv_arry[], int n) {
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << argv_arry[i] << ", ";
+	}
+}
 
 void RaceMgr::run()
 {
-	//loop
-	//do {
-		//if (BS.DONE) {
-		BS.step();
-		BS.print();
-		//}
-		//}
-		/*
-		if (MS.DONE) {
-			MS.step();
-			MS.print();
-		*/
+	//0 is not done
+	//1 is done
+	//2 valued moved
 
-	//} while (BS.DONE && MS.DONE);
-		
+		while (BS.get_state != 1 /*|| !MS.Done*/) {
 
-	//loop
-		//MS.step();
-		//MS.print();
-	//cout << "Geekname is: " << geekname << endl;
+			if (BS.get_state != 1) {
+
+				BS.step();
+
+				if(BS.get_state == 2){
+					cout << "#" << BS.get_step_count << ",  BS:  [";
+					//print();
+					cout << "]" << endl;
+				}
+			}/*
+			if (!MS.is_done()) {
+				MS.step();
+				msTurnsItTook++;
+				mergeSortCallCount++;
+				//if(moved){
+					cout << "#" << mergeSortCallCount << ",  MS:  [";
+					//print();
+					cout << "]" << endl;
+				}
+			}*/
+		}
+		cout << "BubbleSort took " << BS.get_step_count << " turns to finish." << endl;
+		cout << "MergeSort took " << msTurnsItTook << " turns to finish." << endl;
 }
 
 
 int main() {
-	int input[12];
+	int myarry[MAX_INT];
 	string InputArray;
 
-	int myarry[12];
 	cout << "Enter Integer array to be sorted (MAX: 12): ";
 	getline(cin, InputArray);
 
 	stringstream ss(InputArray);
 	string s;
-	int i = 0;
+	int array_size = 0;
 	
 	while (getline(ss, s, ' ')) {
-		myarry[i] = stoi(s);
-		i++;
+		myarry[array_size] = stoi(s);
+		array_size++;
 	}
-	int array_size = i;
 
 	RaceMgr Mgr(myarry, array_size);
 	Mgr.run();
